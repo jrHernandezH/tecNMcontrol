a20f81da2b584c2230b5442db333a72b4abc82d9
DevBobby <L20011247@orizaba.tecnm>
2023-12-22 20:04:23 -0600

    Creacion del sidebar ideal para la landyng

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,31 +1,108 @@
-import React from 'react'
-import '../styles/header/header.css';
+import React, { useRef } from 'react'
+import '../styles/header/header.css'
+import 'boxicons/css/boxicons.min.css'
 import { NavLink } from 'react-router-dom';
 
-function Head() {
 
+function Head({containerRef}) {
+  //Referencias para no ocupar el document.getByElemnt.ID en cambio react nos proporciona lo que es useRef
+  const sidebarRef =  useRef();
+  const toggleRef = useRef();
+  const searchRef = useRef();
+  const modeRef = useRef();
+  const modeTexRef = useRef(); 
+  
+  //Funciones que ocupa el menu
+  const modeSwitch =()=>{
+    containerRef.current.classList.toggle('dark')
+  }
+  const toggle=()=>{
+    sidebarRef.current.classList.toggle('close')  
+  }
   return (
     <>
-        <header className='header'>
-            <NavLink to="/"><h1 className='title-page'>TecNMControl</h1></NavLink>
-            <nav className='nav'>
-                <div className="menu">
-                    <label htmlFor="toggle">Menú</label>
-                    <input type="checkbox" id='toggle'/>
-                    <div className="routes">
-                        <NavLink to="/" id='Home'>Home</NavLink>
-                        <NavLink to="/About" id='about'>About</NavLink>
-                        <NavLink to="/Contact" id='contact'>Contact</NavLink>
-                    </div>
-                </div>
-                <div className='container-login' id='container-login'>
-                    <NavLink to="/login/student" id='students'>Estudiantes</NavLink>
-                    <NavLink to="/login/teacher" id='teacher'>Profesores</NavLink>
-                </div>
-            </nav>
+      <nav className="sidebar" ref={sidebarRef}>
+        <header>
+          <div className="image-text">
+            
+
+            <div className="text header-text">
+              <span className="name">Landyn</span>
+              <span className="profession">Web Developer</span>
+            </div>
+          </div>
+          <i className="bx bx-chevron-right toggle" ref={toggleRef} onClick={toggle}></i>
         </header>
+        <div className="menu-bar">
+          <div className="menu">
+            <li className="search-box" ref={searchRef}>
+              <i className="bx bx-search icon"></i>
+              <input type="search" placeholder="search..." />
+            </li>
+            <ul className="menu-links">
+              <li className="nav-link">
+                <NavLink>
+                  <i className="bx bx-home-alt icon"></i>
+                  <span className="text nav-text">Home</span>
+                </NavLink>
+              </li>
+              <li className="nav-link">
+                <NavLink>
+                  <i className="bx bx-info-circle icon"></i>
+                  <span className="text nav-text">About</span>
+                </NavLink>
+              </li>
+              <li className="nav-link">
+                <NavLink>
+                  <i className="bx bx-envelope icon"></i>
+                  <span className="text nav-text">Contact</span>
+                </NavLink>
+              </li>
+              <li className="nav-link">
+                <NavLink>
+                  <i className="bx bx-briefcase icon"></i>
+                  <span className="text nav-text">Carrers</span>
+                </NavLink>
+              </li>
+              <li className="nav-link">
+                <NavLink>
+                  <i className="bx bx-book icon"></i>
+                  <span className="text nav-text">Master's</span>
+                </NavLink>
+              </li>
+              <li className="nav-link">
+                <NavLink>
+                  <i className="bx bx-rocket icon"></i>
+                  <span className="text nav-text">Specialization</span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+
+          <div className="bottom-content">
+            <li className="">
+              <NavLink>
+                <i className="bx bx-log-in icon"></i>
+                <span className="text nav-text">Loggin</span>
+              </NavLink>
+            </li>
+            <li className="mode">
+              <div className="moon-sun">
+                <i className="bx bx-moon icon moon"></i>
+                <i className="bx bx-sun icon sun"></i>
+              </div>
+              
+              <span className="mode-text text" ref={modeTexRef}>Dark Mode</span>
+
+              <div className="toggle-switch" ref={modeRef} onClick={modeSwitch}>
+                <span className="switch" ></span>
+              </div>
+            </li>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
 export default Head
--- src/views/Landyn.jsx
+++ src/views/Landyn.jsx
@@ -1,20 +1,12 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Head from '../components/Head'
-import Home from '../components/Home'
-import Data from '../components/Data'
-import Video from '../components/videos/Video'
-import Footer from '../components/Footer'
 
 function Landyn() {
-  
+  const  containerRef = useRef();
   return (
     <>
-        <div className="container">
-            <Head/>
-            <Home/>
-            <Data/>
-            <Video/>
-            <Footer/>
+        <div className="container" ref={containerRef}>
+            <Head containerRef={containerRef}/>
         </div>
     </>
   )
